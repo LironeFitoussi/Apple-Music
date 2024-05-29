80c93b696bd5c8b1d9ab3fa3ae8e37c68eb1c6b9
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-05-29 19:16:46 +0300

    Done Info container

--- src/components/PlayerVolume/PlayerVolume.jsx
+++ src/components/PlayerVolume/PlayerVolume.jsx
@@ -3,6 +3,10 @@ import { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { playerActions } from "../../store/player-slice";
 
+// SVG Components
+import SVGComponent from "../SVG/volume-svg.jsx";
+import UserSVG from "../SVG/user-svg.jsx";
+
 const PlayerVolume = () => {
   const dispatch = useDispatch();
   const volume = useSelector((state) => state.player.volume);
@@ -14,21 +18,31 @@ const PlayerVolume = () => {
 
   useEffect(() => {
     if (sliderRef.current) {
-      sliderRef.current.style.setProperty('--slider-value', `${volume}%`);
+      sliderRef.current.style.setProperty("--slider-value", `${volume}%`);
     }
   }, [volume]);
 
   return (
-    <div className={styles.playerVolume}>
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value={volume}
-        className={styles.volumeSlider}
-        onChange={handleVolumeChange}
-        ref={sliderRef}
-      />
+    <div className={styles.container}>
+      <div className={styles.playerVolume}>
+        <SVGComponent />
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={volume}
+          className={styles.volumeSlider}
+          onChange={handleVolumeChange}
+          ref={sliderRef}
+        />
+      </div>
+      <div className={styles.infoContainer}>
+        <img src="./images/list.svg" alt="" />
+        <div className={styles.signIn}>
+          <UserSVG />
+          <span>Sign in</span>
+        </div>
+      </div>
     </div>
   );
 };
