ae956927d2a2dfa856e3bbb6e8bcbddaf95c70eb
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-05-30 16:35:41 +0300

    progress

--- src/components/TopShelf/TopShelf.jsx
+++ src/components/TopShelf/TopShelf.jsx
@@ -4,42 +4,77 @@ import VerBoxPlaylist from "../VerBoxPlaylist/VerBoxPlaylist";
 
 const TopShelf = () => {
   return (
-    <div className={styles.topShelf}>
-      <VerBoxPlaylist
-        playlist={{
-          collection: "UPDATED PLAYLIST",
-          title: "Today’s Hits",
-          subtitle: "Apple Music Hits",
-          description:
-            "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
-          cover:
-            "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/ec/cf/d7/eccfd76f-d043-0ba7-a181-cb1f468ef1a9/6ac45cc9-6322-4ccc-b9b0-03e732ef83a5.png/530x304sr.webp",
-        }}
-      />
+    <div className={styles.carousel}>
+      <div className={styles.topShelf}>
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            description:
+              "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/ec/cf/d7/eccfd76f-d043-0ba7-a181-cb1f468ef1a9/6ac45cc9-6322-4ccc-b9b0-03e732ef83a5.png/530x304sr.webp",
+          }}
+        />
 
-      <VerBoxPlaylist
-        playlist={{
-          collection: "UPDATED PLAYLIST",
-          title: "Today’s Hits",
-          subtitle: "Apple Music Hits",
-          description:
-            "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
-          cover:
-            "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/15/de/54/15de544f-aab2-e59c-6289-ff783c3e0b16/da736ff3-75b2-4e04-91bb-ab1d6e971f66.png/530x304sr.webp",
-        }}
-      />
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            description:
+              "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/15/de/54/15de544f-aab2-e59c-6289-ff783c3e0b16/da736ff3-75b2-4e04-91bb-ab1d6e971f66.png/530x304sr.webp",
+          }}
+        />
 
-      <VerBoxPlaylist
-        playlist={{
-          collection: "UPDATED PLAYLIST",
-          title: "Today’s Hits",
-          subtitle: "Apple Music Hits",
-          description:
-            "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
-          cover:
-            "https://is1-ssl.mzstatic.com/image/thumb/Features211/v4/8d/e0/53/8de0534f-bfb2-7bc2-39db-a63da333d06b/source/530x304sr.webp",
-        }}
-      />
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features211/v4/8d/e0/53/8de0534f-bfb2-7bc2-39db-a63da333d06b/source/530x304sr.webp",
+          }}
+        />
+      </div>
+      <div className={styles.topShelf}>
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            description:
+              "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/ec/cf/d7/eccfd76f-d043-0ba7-a181-cb1f468ef1a9/6ac45cc9-6322-4ccc-b9b0-03e732ef83a5.png/530x304sr.webp",
+          }}
+        />
+
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            description:
+              "Shaboozey finds temporary euphoria in “A Bar Song (Tipsy).” Hear how in Spatial Audio.",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/15/de/54/15de544f-aab2-e59c-6289-ff783c3e0b16/da736ff3-75b2-4e04-91bb-ab1d6e971f66.png/530x304sr.webp",
+          }}
+        />
+
+        <VerBoxPlaylist
+          playlist={{
+            collection: "UPDATED PLAYLIST",
+            title: "Today’s Hits",
+            subtitle: "Apple Music Hits",
+            cover:
+              "https://is1-ssl.mzstatic.com/image/thumb/Features211/v4/8d/e0/53/8de0534f-bfb2-7bc2-39db-a63da333d06b/source/530x304sr.webp",
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/components/VerBoxPlaylist/VerBoxPlaylist.jsx
+++ src/components/VerBoxPlaylist/VerBoxPlaylist.jsx
@@ -2,13 +2,18 @@ import styles from "./VerBoxPlaylist.module.css";
 
 const VerBoxPlaylist = ({ playlist }) => {
   const { collection, title, subtitle, description, cover } = playlist;
+
   return (
     <div className={styles.verBoxPlaylist}>
-      <span>{collection}</span>
-      <h3>{title}</h3>
-      <p>{subtitle}</p>
-      <img src={cover} alt={title} />
-      <p>{description}</p>
+      <div className={styles.header}>
+        <span>{collection}</span>
+        <h3>{title}</h3>
+        <p>{subtitle}</p>
+      </div>
+      <div className={styles.imgContainer}>
+        <img src={cover} alt={title} />
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
